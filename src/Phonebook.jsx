fe1c0544e7dd981b3c197b21f1dca982d77781e7
Aleksey Yesin <esinru@gmail.com>
2021-04-25 19:33:36 +0300

    Adds code splitting

--- src/Phonebook.jsx
+++ src/Phonebook.jsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from 'react';
 import { Redirect, Switch } from 'react-router';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
@@ -7,11 +8,12 @@ import Container from './components/Container';
 import AppBar from './components/AppBar';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
-import RegisterView from './views/RegisterView';
-import ContactsView from './views/ContactsView';
-import LoginView from './views/LoginView';
-import HomeView from './views/HomeView';
 import { authOps } from './redux/auth';
+
+const HomeView = lazy(() => import('./views/HomeView'));
+const RegisterView = lazy(() => import('./views/RegisterView'));
+const LoginView = lazy(() => import('./views/LoginView'));
+const ContactsView = lazy(() => import('./views/ContactsView'));
 
 const Phonebook = () => {
     const dispatch = useDispatch();
@@ -24,27 +26,33 @@ const Phonebook = () => {
         <Container>
             <AppBar />
 
-            <Switch>
-                <PublicRoute exact path={routes.home} component={HomeView} />
-                <PublicRoute
-                    path={routes.register}
-                    restricted
-                    redirectTo={routes.contacts}
-                    component={RegisterView}
-                />
-                <PublicRoute
-                    path={routes.login}
-                    restricted
-                    redirectTo={routes.contacts}
-                    component={LoginView}
-                />
-                <PrivateRoute
-                    path={routes.contacts}
-                    redirectTo={routes.login}
-                    component={ContactsView}
-                />
-                <Redirect to={routes.home} />
-            </Switch>
+            <Suspense fallback={<p>Загрузка...</p>}>
+                <Switch>
+                    <PublicRoute
+                        exact
+                        path={routes.home}
+                        component={HomeView}
+                    />
+                    <PublicRoute
+                        path={routes.register}
+                        restricted
+                        redirectTo={routes.contacts}
+                        component={RegisterView}
+                    />
+                    <PublicRoute
+                        path={routes.login}
+                        restricted
+                        redirectTo={routes.contacts}
+                        component={LoginView}
+                    />
+                    <PrivateRoute
+                        path={routes.contacts}
+                        redirectTo={routes.login}
+                        component={ContactsView}
+                    />
+                    <Redirect to={routes.home} />
+                </Switch>
+            </Suspense>
         </Container>
     );
 };
